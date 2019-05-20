8f39f773186cebc55f2533194fab87339cb9ee7f
Mickaël Canouil <mickael.canouil@cnrs.fr>
2019-05-20 19:43:40 +0200

    add cleaning

--- _build.sh
+++ _build.sh
@@ -8,5 +8,8 @@ pdflatex curriculum_vitae
 
 rm *.aux *.bbl *.log *.blg *.out
 
+rm -R curriculum_vitae_files
+rm *.html
+
 
 Rscript -e 'rmarkdown::render(input = "curriculum_vitae.Rmd", output_file = "index.html", encoding = "UTF-8")'
