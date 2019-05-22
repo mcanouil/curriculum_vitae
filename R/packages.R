a4e520c3cee16ace6fc8f440e46cd19a9c8c80dc
Mickaël Canouil <mickael.canouil@cnrs.fr>
2019-05-22 17:07:54 +0200

    add common format function between website and resume

---
+++ R/packages.R
@@ -0,0 +1,38 @@
+rbind(
+  data.frame(
+    name = "CARoT",
+    title = "Centralised and Automated RepOrting Tools",
+    authors = "Mickaël Canouil, Mathilde Boissel and Lijiao Ning",
+    where = "GitHub",
+    user = "mcanouil",
+    since = "2019-04", 
+    stringsAsFactors = FALSE
+  ),
+  data.frame(
+    name = "NACHO",
+    title = "NanoString Quality Control Dashboard",
+    authors = "Mickaël Canouil, Gerard Bouland and Roderick Slieker",
+    where = "CRAN",
+    user = "mcanouil",
+    since = "2019-04", 
+    stringsAsFactors = FALSE
+  ),
+  data.frame(
+    name = "clere",
+    title = "Simultaneous Variables Clustering and Regression",
+    authors = "Loïc Yengo, Julien Jacques, Christophe Biernacki and Mickaël Canouil",
+    where = "CRAN",
+    user = "mcanouil",
+    since = "2014-02", 
+    stringsAsFactors = FALSE
+  ),
+  data.frame(
+    name = "snpEnrichment",
+    title = "SNPs Enrichment Analysis",
+    authors = "Mickaël Canouil and Loïc Yengo",
+    where = "CRAN",
+    user = "mcanouil",
+    since = "2013-04", 
+    stringsAsFactors = FALSE
+  )
+)
