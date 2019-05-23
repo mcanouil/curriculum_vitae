d05b9f6333d483b6a1cf4793e6cc1f4dc3a0b7cc
Mickaël Canouil <mickael.canouil@cnrs.fr>
2019-05-23 16:43:39 +0200

    cv as part of a website

--- _build.sh
+++ _build.sh
@@ -1,18 +1,14 @@
 #!/bin/sh
 
 cd latex/
-
 pdflatex curriculum_vitae
 bibtex curriculum_vitae
 pdflatex curriculum_vitae 
 pdflatex curriculum_vitae 
 rm *.aux *.bbl *.log *.blg *.out
-
 cd ../
 
-rm *.html
 
+Rscript -e 'rmarkdown::render(input = "curriculum_vitae/curriculum_vitae.Rmd", output_file = "cv.html", encoding = "UTF-8")'
 
-Rscript -e 'rmarkdown::render(input = "curriculum_vitae.Rmd", output_file = "index.html", encoding = "UTF-8")'
-
-Rscript -e 'rmarkdown::render_site(input = "www")'
+Rscript -e 'rmarkdown::render_site()'
