c40bad73880afb85ae3c82c79d2670fa4a088ecf
Mickaël Canouil <mickael.canouil@cnrs.fr>
2019-05-29 14:48:38 +0200

    add rm cv html

--- _build.sh
+++ _build.sh
@@ -8,6 +8,7 @@ pdflatex curriculum_vitae
 rm *.aux *.bbl *.log *.blg *.out
 cd ../
 
+rm curriculum_vitae/curriculum_vitae.html
 
 Rscript -e 'rmarkdown::render(input = "curriculum_vitae/curriculum_vitae.Rmd", output_dir = ".", output_file = "cv.html", encoding = "UTF-8")'
 
