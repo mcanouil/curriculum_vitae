3ef4314dc90f8a341d0ee957df602ead52a1bb8b
Mickaël Canouil <mickael.canouil@cnrs.fr>
2019-07-19 15:11:24 +0200

    Build update

--- _build.sh
+++ _build.sh
@@ -3,8 +3,8 @@
 cd latex/
 pdflatex curriculum_vitae
 bibtex curriculum_vitae
-pdflatex curriculum_vitae 
-pdflatex curriculum_vitae 
+pdflatex curriculum_vitae
+pdflatex curriculum_vitae
 rm *.aux *.bbl *.log *.blg *.out
 cd ../
 
