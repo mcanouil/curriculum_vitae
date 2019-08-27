799507cc3f3b6e182fbc0500210be7c4f06d0e9f
Mickaël Canouil <mickael.canouil@cnrs.fr>
2019-08-27 14:00:19 +0200

    new test

--- _deploy.sh
+++ _deploy.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-Rscript -e 'rmarkdown::render(input = "curriculum_vitae/curriculum_vitae.Rmd", encoding = "UTF-8")' ;
+Rscript -e 'rmarkdown::render(input = "curriculum_vitae/curriculum_vitae.Rmd", output_dir = ".", output_file = "cv.html", encoding = "UTF-8")' ;
 
-Rscript -e 'rmarkdown::render_site()' ;
+echo "curl -u $1:$2 -T cv.html ${3}index.html ;";
 
-echo "curl -u $1:$2 -T curriculum_vitae/curriculum_vitae.html ${3}index.html ;";
-ping ${3}index.html
+ping ${3}
+
+
+# Rscript -e 'rmarkdown::render_site()' ;
 
 # find -name '*.html' -type f -exec curl -u $1:$2 -T {} ${3}dev/ \;
