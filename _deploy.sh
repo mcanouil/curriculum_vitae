8c552241ca05583b562eb18f63e2aa78fb620683
Mickaël Canouil <mickael.canouil@cnrs.fr>
2019-08-27 18:22:45 +0200

    new article

--- _deploy.sh
+++ _deploy.sh
@@ -2,7 +2,7 @@
 
 Rscript -e 'rmarkdown::render(input = "curriculum_vitae/curriculum_vitae.Rmd", output_dir = ".", output_file = "cv.html", encoding = "UTF-8")' ;
 
-curl -u $1:$2 -T cv.html $3 ;
+curl -u $1:$2 -T cv.html $3/index.html ;
 
 # Rscript -e 'rmarkdown::render_site()' ;
 # 
