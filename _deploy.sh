96660af11a4a19f9571c0f00668fcca11b5ee2ff
Mickaël Canouil <mickael.canouil@cnrs.fr>
2019-08-27 13:27:12 +0200

    deploy script

---
+++ _deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+Rscript -e 'rmarkdown::render(input = "curriculum_vitae/curriculum_vitae.Rmd", encoding = "UTF-8")' ;
+
+Rscript -e 'rmarkdown::render_site()' ;
+
+curl -u $1:$2 -T curriculum_vitae/curriculum_vitae.html ${3}index.html ;
+
+find -name '*.html' -type f -exec curl -u $1:$2 -T {} ${3}dev/ \;
