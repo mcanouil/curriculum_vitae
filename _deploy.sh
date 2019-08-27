f692bcfcf441e16faa465638baed1ccd6a8ffd9c
Mickaël Canouil <mickael.canouil@cnrs.fr>
2019-08-27 17:01:01 +0200

    new travis env

--- _deploy.sh
+++ _deploy.sh
@@ -2,11 +2,8 @@
 
 Rscript -e 'rmarkdown::render(input = "curriculum_vitae/curriculum_vitae.Rmd", output_dir = ".", output_file = "cv.html", encoding = "UTF-8")' ;
 
-echo "curl -u $1:$2 -T cv.html ${3}index.html ;";
+curl -u $1:$2 -T cv.html $3 ;
 
-ping ${3}
+Rscript -e 'rmarkdown::render_site()' ;
 
-
-# Rscript -e 'rmarkdown::render_site()' ;
-
-# find -name '*.html' -type f -exec curl -u $1:$2 -T {} ${3}dev/ \;
+find -name '*.html' -type f -exec curl -u $1:$2 -T {} $4 \;
