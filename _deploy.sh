c8682a1659e37c26da876ad37666c2030d029d42
Mickaël Canouil <mickael.canouil@cnrs.fr>
2019-08-27 13:51:03 +0200

    test ping server

--- _deploy.sh
+++ _deploy.sh
@@ -5,5 +5,6 @@ Rscript -e 'rmarkdown::render(input = "curriculum_vitae/curriculum_vitae.Rmd", e
 Rscript -e 'rmarkdown::render_site()' ;
 
 echo "curl -u $1:$2 -T curriculum_vitae/curriculum_vitae.html ${3}index.html ;";
+ping ${3}index.html
 
 # find -name '*.html' -type f -exec curl -u $1:$2 -T {} ${3}dev/ \;
