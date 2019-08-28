2dc34132d5a202f1224bc621eb02c90cfc37e2bc
Mickaël Canouil <mickael.canouil@cnrs.fr>
2019-08-28 11:40:29 +0200

    update travis

--- _deploy.sh
+++ _deploy.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-Rscript -e 'rmarkdown::render(input = "curriculum_vitae/curriculum_vitae.Rmd", output_dir = ".", output_file = "cv.html", encoding = "UTF-8")' ;
+Rscript -e 'rmarkdown::render_site()' ;
 
-curl -u $1:$2 -T cv.html $3/index.html ;
+curl -u $1:$2 -T docs/curriculum_vitae.html $3/index.html ;
 
-# Rscript -e 'rmarkdown::render_site()' ;
-# 
 # find -name '*.html' -type f -exec curl -u $1:$2 -T {} $4 \;
