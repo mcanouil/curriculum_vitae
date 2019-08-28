a9e29ccbc354a72ee06d1998c43b5ae2194fa75a
Mickaël Canouil <mickael.canouil@cnrs.fr>
2019-08-28 17:59:22 +0200

    remove old deploy

--- _deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-Rscript -e 'rmarkdown::render_site(); unlink(c("_site/README.html", "_site/DESCRIPTION"))' ;
-
-curl -u $1:$2 -T _site/curriculum_vitae.html $3/index.html ;
-
-find -name '*.html' -type f -exec curl -u $1:$2 -T {} $4 \;
