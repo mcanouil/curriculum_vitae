44f2a685564696dfce7610568d0c34b4d0353a95
Mickaël Canouil <mickael.canouil@cnrs.fr>
2020-04-27 15:49:16 +0200

    try to fix deploy function

--- utils/deploy.R
+++ utils/deploy.R
@@ -1,6 +1,7 @@
 deploy_site <- function(
   pkg = ".",
   ssh_id = Sys.getenv("id_rsa", ""),
+  host = "github.com",
   repo_slug = Sys.getenv("TRAVIS_REPO_SLUG", ""),
   commit_message = "",
   verbose = FALSE,
@@ -31,7 +32,7 @@ deploy_site <- function(
   #   gh <- rematch2::re_match(pkg$github_url, github_url_rx())
   #   repo_slug <- paste0(gh$owner, "/", gh$repo)
   # }
-  pkgdown:::github_clone(dest_dir, repo_slug)
+  pkgdown:::git("remote", "set-url", "origin", sprintf("git@%s:%s.git", host, repo_slug))
   rmarkdown::render(
     input = "curriculum_vitae.Rmd", 
     output_file = "index.html", 
@@ -43,7 +44,8 @@ deploy_site <- function(
       picture = "data/cv.png"
     )
   )
+  
   commit_message <- sprintf("Built site for %s: %s@%s", commit_message, Sys.Date(), substr(Sys.getenv("TRAVIS_COMMIT"), 1, 7))
-  pkgdown:::github_push(dest_dir, commit_message)
+  pkgdown:::github_push(dest_dir, commit_message, "origin", "gh-pages")
   cli::rule("Deploy completed", line = 2)
 }
